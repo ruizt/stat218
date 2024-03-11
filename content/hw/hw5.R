57bc6db8a0fd931d5c3ccbb86a57a5c32aba664a
ruizt <75809313+ruizt@users.noreply.github.com>
2024-03-11 12:09:33 -0700

    update calendly link

--- content/hw/hw5.R
+++ content/hw/hw5.R
@@ -1,7 +1,6 @@
 ## ----libraries--------------------------------------------------------------------------
-library(epitools)
-library(Sleuth3)
 library(openintro)
+library(epitools)
 
 
 ## ----prenatal vitamins and autism-------------------------------------------------------
@@ -10,8 +9,6 @@ vitamin <- matrix(data = c(111, 70, 143, 159),
                   dimnames = list(vitamin = c('no vitamin', 'vitamin'), 
                                   autism = c('autism', 'no autism')))
 
-oddsratio(vitamin, rev = 'columns')
-
 
 ## ----tea and esophageal carcinoma-------------------------------------------------------
 tea <- matrix(data = c(47 - 17, 17, 824 - 283, 283),
@@ -19,12 +16,7 @@ tea <- matrix(data = c(47 - 17, 17, 824 - 283, 283),
               dimnames = list(tea = c('tea', 'no tea'),
                               carcinoma = c('no carcinoma', 'carcinoma')))
 
-oddsratio(tea)
 
+## ----malaria vaccine--------------------------------------------------------------------
 
-## ---------------------------------------------------------------------------------------
-malaria.tbl <- xtabs(~ treatment + outcome, data = malaria)
-
-malaria.est <- riskratio(malaria.tbl, rev = 'columns')
-1 - malaria.est$measure[2, 1]
-
+head(malaria)
